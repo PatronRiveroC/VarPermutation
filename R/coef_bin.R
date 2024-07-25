361777e2b2d66db51d92f1e9e50e9c4b1aa26661
PatronRiveroC <85257325+PatronRiveroC@users.noreply.github.com>
2024-07-25 11:17:21 -0600

    Update coef_bin.R

--- R/coef_bin.R
+++ R/coef_bin.R
@@ -2,10 +2,7 @@ coef_bin <- function(df, num_variables) {
   if (num_variables > ncol(df)) {
     stop("Variables exceed the number of columns in df")
   }
-  column_combinations <- list()
-  for (r in 8:num_variables) {
-    column_combinations <- c(column_combinations, combn(names(df), r, simplify = FALSE))
-  }
+  column_combinations <- combn(names(df), num_variables, simplify = FALSE)
   subset_dfs <- lapply(column_combinations, function(cols) df[, cols, drop = FALSE])
   return(subset_dfs)
 }
